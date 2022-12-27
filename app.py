10d9682962710cda33334b91e33175080533ed80
rockyhg <hrk.haga@gmail.com>
2022-12-27 22:13:18 +0900

    HTMLに画像パスを渡す

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template  # , request
 
 # from models.models import get_date_list
-from news_wc import NewsWordcloud
+from news_wc import NewsWordcloud, get_news_words
 
 
 app = Flask(__name__)
@@ -10,10 +10,11 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     # date_list = get_date_list()
-    news_wc = NewsWordcloud()
-    wc_img_data = news_wc.to_base64()
+    words = get_news_words()
+    news_wc = NewsWordcloud(words, file_save=True)
+    # wc_img_data = news_wc.to_base64()
     # return render_template("index.html", todays_wc=wc_img_data, date_list=date_list)
-    return render_template("index.html", todays_wc=wc_img_data)
+    return render_template("index.html", todays_wc=news_wc.save_path)
 
 
 # @app.route("/", methods=["POST"])
--- news_wc.py
+++ news_wc.py
@@ -15,23 +15,23 @@ NEWS_DIR = "./models/news"
 FONT_PATH = "./static/fonts/NotoSansJP-Bold.otf"
 MASK_PATH = "./static/images/cloud.jpeg"
 # "./static/images/bubble.jpeg"
+WC_DIR = "./static/images"
 TODAY_WC_PATH = "./static/images/today_wc.png"
 PAST_WC_PATH = "./static/images/past_wc.png"
 
 
 class NewsWordcloud:
     """ワードクラウドを生成し、画像ファイルに保存する
-    news_date: str
-        ニュースの日付 ("yyyy-mm-dd")"""
-    def __init__(self, date_str: str = None, file_save: bool = False) -> None:
-        if date_str:
-            self.date_str = date_str
-            self.save_path = PAST_WC_PATH
-        else:
-            self.date_str = date.today().strftime("%Y-%m-%d")
-            self.save_path = TODAY_WC_PATH
-
-        self.news_words = self._set_news_words()
+    news_words: str
+        ニュースの単語（スペース区切り）
+    date_str: str
+        ニュースの日付 (YYYY-MM-DD) -> ファイル名"""
+    # def __init__(self, date_str: str = None, file_save: bool = False) -> None:
+    def __init__(self, news_words: str, date_str: str = None, file_save: bool = True) -> None:
+        if not date_str:
+            date_str = date.today().strftime("%Y-%m-%d")
+        self.save_path = os.path.join(WC_DIR, f"wc_{date_str}.png")
+        self.news_words = news_words
 
         # TF-IDFで重み付け
         vectorizer = TfidfVectorizer()
@@ -61,18 +61,6 @@ class NewsWordcloud:
             self._clean_cache()
             wc.to_file(self.save_path)
 
-    def _set_news_words(self) -> str:
-        """DBから news_words を取得"""
-        dir = f"{NEWS_DIR}/{self.date_str}"
-        news_file_list = glob(os.path.join(dir, "*.csv"))
-        words_list = []
-        for news_file in news_file_list:
-            with open(news_file, "r") as f:
-                df_news = pd.read_csv(f)
-            words = df_news["words"].to_list()
-            words_list.append(" ".join(words))
-        return " ".join(words_list)
-
     def _choose_colormap(self) -> str:
         colormap_candidate = ["viridis", "plasma", "inferno", "magma", "cividis"]
         return random.choice(colormap_candidate)
@@ -91,7 +79,25 @@ class NewsWordcloud:
         return base64_data
 
 
+def get_news_words(date_str: str = None) -> str:
+    """DBから news_words を取得
+    date_str: str
+        ニュースの日付 (YYYY-MM-DD)"""
+    if not date_str:
+        date_str = date.today().strftime("%Y-%m-%d")
+    file_path = os.path.join(NEWS_DIR, date_str, "*.csv")
+    news_file_list = glob(file_path)
+    words_list = []
+    for news_file in news_file_list:
+        with open(news_file, "r") as f:
+            df_news = pd.read_csv(f)
+        words = df_news["words"].to_list()
+        words_list.append(" ".join(words))
+    return " ".join(words_list)
+
+
 if __name__ == "__main__":
-    news_wc = NewsWordcloud(file_save=True)
+    words = get_news_words()
+    news_wc = NewsWordcloud(words, file_save=True)
     # img_data = news_wc.to_base64()
     # print(img_data)
