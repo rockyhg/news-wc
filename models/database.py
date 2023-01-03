296b0e26ced77c1e0aa489a9d252ce69cc0b77af
rockyhg <hrk.haga@gmail.com>
2023-01-04 02:50:53 +0900

    環境変数を変更 (DATABASE_URL2)

--- models/database.py
+++ models/database.py
@@ -6,9 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), "news.sqlite")
-database_url = os.environ.get('DATABASE_URL')
-if database_url:
-    database_url = 'postgresql://' + database_url.split(sep='://')[1]
+database_url = os.environ.get("DATABASE_URL2")
+# if database_url:
+#     database_url = 'postgresql://' + database_url.split(sep='://')[1]
 
 engine = create_engine(
     database_url or ("sqlite:///" + databese_file),
