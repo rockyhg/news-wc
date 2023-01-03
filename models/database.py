3d944f6271d157f0944c6d897fac48fe311c7567
rockyhg <hrk.haga@gmail.com>
2023-01-04 01:47:20 +0900

    PosgreSQLのURLを修正

--- models/database.py
+++ models/database.py
@@ -5,12 +5,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-DATABASE_URL = "postgresql://nfgauqauvwafae:cddeab18de5706a0bd700dad56c59a5f81c72d5e204cb058aced438d1f4ac624@ec2-52-54-212-232.compute-1.amazonaws.com:5432/d6klbef1mfhlj7"
-
 databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), "news.sqlite")
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    database_url = 'postgresql://' + database_url.split(sep='://')[1]
+
 engine = create_engine(
-    # DATABASE_URL,
-    "sqlite:///" + databese_file,
+    database_url or ("sqlite:///" + databese_file),
     convert_unicode=True,
     echo=False,
 )
