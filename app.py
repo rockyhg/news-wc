e86b6cc8ac51bf7d710a17be0f1eaa46a9610aa8
rockyhg <hrk.haga@gmail.com>
2023-01-04 20:02:27 +0900

    ニュース生成機能を追加

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 from news_wc import NewsWordcloud, get_words_from_db
-from utils import jp_today
+from utils import jp_today, generate_news_text
 
 app = Flask(__name__)
 
@@ -11,12 +11,30 @@ def index():
     words = get_words_from_db()
     news_wc = NewsWordcloud(words)
     wc_img_data = news_wc.to_base64()
+    news_list = news_wc.high_rank_news
+    high_rank_words = " ".join([item["word"] for item in news_wc.high_rank_news])
+    text = generate_news_text(high_rank_words)
+
     return render_template(
         "index.html",
         todays_date=jp_today().strftime("%Y-%m-%d"),
         todays_wc=wc_img_data,
-        news_list=news_wc.high_rank_news
+        news_list=news_list,
+        words_for_gen=high_rank_words,
+        generated_text=text
     )
+
+
+# @app.route("/generate", methods=["GET"])
+# def update():
+#     text = generate_news_text(high_rank_words)
+#     return render_template(
+#         "generate.html",
+#         todays_date=jp_today().strftime("%Y-%m-%d"),
+#         todays_wc=wc_img_data,
+#         news_list=news_list,
+#         generated_text=text
+#     )
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+import os
 from datetime import datetime, timedelta
+
+import openai
 
 
 def jp_time_now():
@@ -7,3 +10,19 @@ def jp_time_now():
 
 def jp_today():
     return jp_time_now().date()
+
+
+def generate_news_text(words: str) -> str:
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    # openai.organization = "org-y155exy2X3GLXAAxqJwgQWxr"
+    prompt = f'''単語: 東京証券取引所 大発会 株価 値下がり 取り引き 滑り出し
+ニュース: 東京証券取引所でことし最初の取り引きが始まり、恒例の大発会で出席者らが活発な取り引きを祈願しました。ただ、株価は値下がりし、厳しい滑り出しとなっています。
+単語: {words}
+ニュース:'''
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        temperature=0.6,
+        max_tokens=256,
+    )
+    return response['choices'][0]['text'].strip()
